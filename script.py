fd65a356552c55ae80fcd9e5eaa46deb3e2d7237
Shubham Singh Kalyanwat <shubham16598@gmail.com>
2020-04-28 18:10:51 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,4 @@
+import os
+cmd="sed -i 's/tn:rni.entity.person.1\ttn:rni.person.literal.emp_id\t\"128482\"/tn:rni.entity.person.2\ttn:rni.person.literal.role\t\"00000\"/' text.txt"
+os.system(cmd)
+
